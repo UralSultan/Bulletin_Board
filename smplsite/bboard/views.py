56bde67a8c95d7c3d1ad45abe35ff9b6c0093c9b
Mac M1 <macm1@Macs-MacBook-Pro.local>
2022-11-01 23:13:10 +0600

    initial commit

---
+++ smplsite/bboard/views.py
@@ -0,0 +1,56 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Bb, Rubric
+from django.template import loader
+from django.views.generic.edit import CreateView
+from .forms import BbForm
+
+
+# 1 этап
+# def index(request):
+#     return HttpResponse("Coming soon")
+
+
+# 2 этап
+# def index(request):
+#     s = 'Список объявлений\r\n\r\n\r\n'
+#     for bb in Bb.objects.order_by('-published'):
+#         s += bb.title + '\r\n' + bb.content + '\r\n\r\n'
+#     return HttpResponse(s, content_type='text/plain; charset=utf-8')
+
+#3 этап
+# Низкоуровневый метод
+# def index(request):
+#     template = loader.get_template('bboard/index.html')
+#     bbs = Bb.objects.order_by('-published')
+#     context = {'bbs': bbs}
+#     return HttpResponse(template.render(context, request))
+
+#4 этап
+#Высокоуровневый метод
+
+# def index(request):
+#     bbs = Bb.objects.order_by('-published')
+#     return render(request, 'bboard/index.html', {'bbs': bbs})
+
+# 5
+# def index(request):
+#     bbs = Bb.objects.all()
+#     return render(request, 'bboard/index.html', {'bbs': bbs})
+
+# 6
+def index(request):
+    bbs = Bb.objects.all()
+    rubrics = Rubric.objects.all()
+    context = {'bbs': bbs, 'rubrics': rubrics}
+    return render(request, 'bboard/index.html', context)
+
+
+def by_rubric(request, rubric_id):
+    bbs = Bb.objects.filter(rubric=rubric_id)
+    rubrics = Rubric.objects.all()
+    current_rubric = Rubric.objects.get(pk=rubric_id)
+    context = {'bbs': bbs, 'rubrics': rubrics, 'current_rubric': current_rubric}
+    return render(request, 'bboard/by_rubric.html', context)
+
+
